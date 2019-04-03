c308d9156380a2769176359982b3425dcf28c250
José Manuel Nieto <superjmn@outlook.com>
2019-04-03 18:32:50 +0200

    Update Collect-Logs.ps1
    Fix for function not found

--- Collect-Logs.ps1
+++ Collect-Logs.ps1
@@ -1,17 +1,6 @@
 [cmdletbinding()]
 param ([Parameter(mandatory=$true)][string] $drive)
 
-$tempFolder = New-TemporaryFile | %{ rm $_; mkdir $_ }
-
-CustomCopy (Join-Path $drive "Windows\Panther\Setupact.log") (Join-Path $tempFolder "Panther")
-CustomCopy (Join-Path $drive "Windows\Panther\Setuperr.log") (Join-Path $tempFolder "Panther")
-CustomCopy (Join-Path $drive "Windows\Panther\Setup.exe\Setupact.log") (Join-Path $tempFolder "Panther")
-CustomCopy (Join-Path $drive "Windows\Panther\Setup.exe\Setuperr.log") (Join-Path $tempFolder "Panther")
-CustomCopy (Join-Path $drive "Windows\MiniDumps\*") (Join-Path $tempFolder "MiniDumps")
-CustomCopy (Join-Path $drive "Windows\CrashDumps\*") (Join-Path $tempFolder "CrashDumps")
-CustomCopy (Join-Path $drive "Windows\MEMORY*.dmp")  (Join-Path $tempFolder "")
-CustomCopy (Join-Path $drive "Windows\inf\*log*") (Join-Path $tempFolder "inf")
-
 function CustomCopy
 {
     param([string]$source, [string]$dest)
@@ -23,6 +12,14 @@ function CustomCopy
 
     Copy-Item $source $dest -ErrorAction SilentlyContinue
 }
+
+$tempFolder = New-TemporaryFile | %{ rm $_; mkdir $_ }
+
+CustomCopy (Join-Path $drive "Windows\Panther\*") (Join-Path $tempFolder "Panther")
+CustomCopy (Join-Path $drive "Windows\MiniDumps\*") (Join-Path $tempFolder "MiniDumps")
+CustomCopy (Join-Path $drive "Windows\CrashDumps\*") (Join-Path $tempFolder "CrashDumps")
+CustomCopy (Join-Path $drive "Windows\MEMORY*.dmp")  (Join-Path $tempFolder "")
+CustomCopy (Join-Path $drive "Windows\inf\*log*") (Join-Path $tempFolder "inf")
 
 Compress-Archive -Path "$($tempFolder)\*" -DestinationPath "Logs.zip"
 Remove-Item $tempFolder -Recurse -Force
