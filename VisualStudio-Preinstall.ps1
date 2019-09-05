08f3813ddc861e4143507a5cb614de6b162b6fff
José Manuel Nieto <superjmn@outlook.com>
2019-09-05 23:07:44 +0200

    Add files via upload

---
+++ VisualStudio-Preinstall.ps1
@@ -0,0 +1,23 @@
+﻿function Map([string] $source, [string]$destination) 
+{
+    Remove-Item $source -Recurse -Force
+    Remove-Item $destination -Recurse -Force
+    New-Item -ItemType Directory -Path $destination
+    cmd /c mklink /j $source $destination
+}
+
+$drive = "F:\"
+
+$paths = @(
+    "Program Files (x86)\Microsoft SDKs",
+    "Program Files (x86)\Windows Kits",
+    "Program Files (x86)\Microsoft Visual Studio"
+)
+
+foreach ($path in $paths) 
+{
+    $source = Join-Path "c:" $path
+    $dest = Join-Path $drive $path
+    Map $source $dest
+}
+
