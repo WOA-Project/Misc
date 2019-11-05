18121bad68014e08b1ff7f5e45d852a7a6054186
José Manuel Nieto <superjmn@outlook.com>
2019-11-05 12:21:12 +0100

    Add files via upload

---
+++ Offload.ps1
@@ -0,0 +1,28 @@
+﻿[CmdletBinding()]
+param(
+    [Parameter(mandatory=$true)]
+    [string]$SourceFolder,
+    [Parameter(mandatory=$true)]
+    [string]$DestinationDrive
+)
+
+if (Test-Path $SourceFolder)
+{
+    $source = Get-Item $SourceFolder
+} else 
+{
+    $source = New-Item $Sourcefolder -ItemType Directory
+}
+
+
+$noQualifierPath = Split-Path $source.FullName -NoQualifier
+$destFolder = Join-Path $DestinationDrive $noQualifierPath
+    
+if (!(Test-Path $destFolder))
+{
+    New-Item -Path $destFolder -ItemType Directory
+}
+
+Copy-Item $source -Destination $destFolder -Recurse
+Remove-Item $source -Recurse -Force
+cmd /c mklink /j $SourceFolder $destFolder
